d0cedef715f8f934b04197880c6ff709aaf94de1
MatyR <matiasgrajmilevich@gmail.com>
2024-09-20 10:37:42 -0300

    Terminado el Projecto 2

--- Projecto 2 Lectura Logs/main.py
+++ Projecto 2 Lectura Logs/main.py
@@ -1,9 +1,19 @@
 #projecto 2: lectura de Logs
+
+from datetime import datetime
+import os
+import csv
+clear = lambda: os.system('cls')
+date_format = "%d/%b/%Y:%H:%M:%S %z"
+
 i = 1
+listIps = [[]]
+clear()
+
 with open("D:\Github\MiniP Python\Projecto 2 Lectura Logs\logs.txt", 'r') as file:
     lista = file.readlines()
     for line in lista:
-        print(f"Linea {i}")
+        
         pointer = 0
         ip = line[pointer:(line.find(" - - "))]
         pointer = line.find(" - - ")+6
@@ -14,10 +24,26 @@ with open("D:\Github\MiniP Python\Projecto 2 Lectura Logs\logs.txt", 'r') as fil
         numError = line[pointer:pointer+3]
         pointer = pointer+4
         sizeAns = line[pointer:-1]
-        
-        print(f"ip: {ip}")
-        print(f"date: {date}")
-        print(f"error: {error}")
-        print(f"numError: {numError}")
-        print(f"size of the answer: {sizeAns}")
+
         i += 1
+        fechaformateada = datetime.strptime(date, date_format)
+        for n in range(len(listIps)+1):
+            if(0 <= n < len(listIps)):
+                try:   
+                    if listIps[n][0] == ip:
+                        listIps[n][1] += 1
+                        listIps[n].append(fechaformateada)
+                        break
+                except:
+                    listIps[0].append(f"{ip}")
+                    listIps[0].append( 1)
+                    listIps[0].append(fechaformateada)
+                    break
+            else:
+                listIps.append([f"{ip}", 1,fechaformateada])
+
+
+with open("results.csv", mode='w') as wFile:
+    writer = csv.writer(wFile, delimiter='|')
+    for n in range(len(listIps)):
+        writer.writerow(listIps[n])
