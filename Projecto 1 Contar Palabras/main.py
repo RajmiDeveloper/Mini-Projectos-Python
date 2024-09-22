dabfdda2123904276158518ecf49074e93837d56
MatyR <matiasgrajmilevich@gmail.com>
2024-09-22 17:04:58 -0300

    Mejora en el Projecto 1 para detectar mas separadores de palabras

--- Projecto 1 Contar Palabras/main.py
+++ Projecto 1 Contar Palabras/main.py
@@ -6,13 +6,13 @@ clear()
 print("ingrese un texto")
 inp = input()
 list = []
+delimeter = [" ", ".", ",", ";"]
 cantP = 0
 spaceReady = True
 for i,s in enumerate(inp):
     if (s != " " and spaceReady == True):
         cantP += 1
         spaceReady = False
-    elif (s == " "):
+    elif (s in delimeter):
         spaceReady = True 
-        
 print(f"Cantidad de palabras: {cantP}")
