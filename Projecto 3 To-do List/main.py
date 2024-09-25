5978c8a27430ae44029f99dc6caf14e1fd51a495
RajmiDeveloper <163068451+RajmiDeveloper@users.noreply.github.com>
2024-09-25 15:23:11 -0300

    To do list csv
    Primera implementación de archivo csv en el 3er proyecto

--- Projecto 3 To-do List/main.py
+++ Projecto 3 To-do List/main.py
@@ -6,7 +6,16 @@ import tkinter as tk
 
 clear = lambda: os.system('cls')
 
-clear()
+clear() 
+
+def escribir ():
+    with open('list.csv','a', newline='') as file:
+        txtInp = toDoEntry.get()
+        
+        writer = csv.writer(file)
+        writer.writerow([str(txtInp), 1])
+
+
 window = tk.Tk()
 window.title("To-do List")
 window.geometry("720x405")
@@ -18,11 +27,9 @@ newTask = tk.Label(window, text = "New Task:", bg="#ffc675")
 newTask.pack()
 toDoEntry = tk.Entry(inputFrame)
 toDoEntry.pack(side='left', padx=10)
-toDoButton = tk.Button(inputFrame, text = "Insert")
+toDoButton = tk.Button(inputFrame, text = "Insert", command = escribir)
 toDoButton.pack(side='right')
 inputFrame.pack(fill=tk.X)
-
-
 
 
 
