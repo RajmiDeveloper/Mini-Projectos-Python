98f3d66a898fe78c85d3ce5e37c2fe5bf19a83e4
MatyR <matiasgrajmilevich@gmail.com>
2024-10-05 10:43:18 -0300

    Creacion del projecto 4

---
+++ Projecto 4 Bases de datos/main.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+import pymysql as ps
+
+
+conect = ps.connect(host='190.7.56.92', user='fotok0_visitor',passwd = 'VisitanteQueLlegaDesdeLaLuna', db='fotok0_dokkan_calculator')
+cur = conect.cursor()
+
+cur.execute('SELECT * from character_info')
+
+for bd in cur:
+    print(bd)
+
+conect.close()
